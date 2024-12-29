22933558d733f918e36f50acfe0938f438176745
Samuel Cuenca <99571985+sami-sopas@users.noreply.github.com>
2024-12-28 21:10:05 -0600

    Create ec2-user-data.sh

---
+++ ec2/ec2-user-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Esta madre es para los datos de usuario que se usan al instanciar un EC2
+
+#Instalar httpd
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hola mundo desde ($hostname -f)</h1>" > /var/www/html/index.html
